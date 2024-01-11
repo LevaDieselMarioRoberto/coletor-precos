b3a30aa30263670a168c62dc819927624468ec92
jpfalcuci <jpfalcuci@gmail.com>
2024-01-11 14:08:58 -0300

    feat: Tratamento de erros básico

--- script_paralelo.py
+++ script_paralelo.py
@@ -7,33 +7,62 @@ from funcoes import imprime_precos, cria_dicionario_para_df
 
 import threading
 import pandas as pd
-# import openpyxl
 from openpyxl import load_workbook
 from openpyxl.styles import Alignment, Font, PatternFill, Border, Side
 from time import time
-from datetime import datetime
 
 global precos_ipr1, precos_ipr2, precos_vbr, precos_rzn, tempo_ex_ipr, tempo_ex_vbr, tempo_ex_rzn
 global precos_gasstation, precos_distrito, precos_itirapua, precos_ppp, precos_pitstop, tempo_ex_ipr_postos
 global precos_vbr_janjao, tempo_ex_vbr_janjao
+global ipiranga_deu_erro, vibra_deu_erro, raizen_deu_erro, ipiranga_postos_deu_erro, vibra_janjao_deu_erro
 
 
 def coleta_e_imprime(preco_func, nome_posto):
-    
+
     global precos_ipr1, precos_ipr2, precos_vbr, precos_rzn, tempo_ex_ipr, tempo_ex_vbr, tempo_ex_rzn
     global precos_gasstation, precos_distrito, precos_itirapua, precos_ppp, precos_pitstop, tempo_ex_ipr_postos
     global precos_vbr_janjao, tempo_ex_vbr_janjao
+    global ipiranga_deu_erro, vibra_deu_erro, raizen_deu_erro, ipiranga_postos_deu_erro, vibra_janjao_deu_erro
 
     if nome_posto == "Ipiranga":
-        precos_ipr1, precos_ipr2, tempo_ex_ipr = preco_func()
+        try: 
+            precos_ipr1, precos_ipr2, tempo_ex_ipr = preco_func()
+            ipiranga_deu_erro = False
+        except: 
+            ipiranga_deu_erro = True     
+            print("Erro ao coletar preços da Ipiranga")
+
     elif nome_posto == "Vibra":
-        precos_vbr, tempo_ex_vbr = preco_func()
+        try: 
+            precos_vbr, tempo_ex_vbr = preco_func()
+            vibra_deu_erro = False
+        except:
+            vibra_deu_erro = True
+            print("Erro ao coletar preços da Vibra")
+
     elif nome_posto == "Raizen":
-        precos_rzn, tempo_ex_rzn = preco_func()
+        try: 
+            precos_rzn, tempo_ex_rzn = preco_func()
+            raizen_deu_erro = False
+        except:
+            raizen_deu_erro = True
+            print("Erro ao coletar preços da Raízen")
+
     elif nome_posto == "Ipiranga Postos":
-        precos_gasstation, precos_distrito, precos_itirapua, precos_ppp, precos_pitstop, tempo_ex_ipr_postos = preco_func()
+        try: 
+            precos_gasstation, precos_distrito, precos_itirapua, precos_ppp, precos_pitstop, tempo_ex_ipr_postos = preco_func()
+            ipiranga_postos_deu_erro = False
+        except:
+            ipiranga_postos_deu_erro = True
+            print("Erro ao coletar preços da Ipiranga (Postos)")
+
     elif nome_posto == "Vibra Janjão":
-        precos_vbr_janjao, tempo_ex_vbr_janjao = preco_func()
+        try: 
+            precos_vbr_janjao, tempo_ex_vbr_janjao = preco_func()
+            vibra_janjao_deu_erro = False
+        except:
+            vibra_janjao_deu_erro = True
+            print("Erro ao coletar preços da Vibra (Janjão)")
 
 
 inicio = time()
@@ -59,6 +88,46 @@ thread_rzn.join()
 thread_ipr_postos.join()
 thread_vbr_janjao.join()
 
+
+# Tratando erros
+combustiveis = ['cif_etanol', 'cif_gasolina', 'cif_gasolina_adt', 'cif_s10', 'cif_s500', 'fob_etanol', 'fob_gasolina', 'fob_gasolina_adt', 'fob_s10', 'fob_s500']
+
+if ipiranga_deu_erro:
+    precos_ipr1, precos_ipr2 = {}, {}
+    for combustivel in combustiveis:
+        precos_ipr1[combustivel] = None
+        precos_ipr2[combustivel] = None
+    tempo_ex_ipr = None
+
+if vibra_deu_erro:
+    precos_vbr = {}
+    for combustivel in combustiveis:
+        precos_vbr[combustivel] = None
+    tempo_ex_vbr = None
+
+if raizen_deu_erro:
+    precos_rzn = {}
+    for combustivel in combustiveis:
+        precos_rzn[combustivel] = None
+    tempo_ex_rzn = None
+
+if ipiranga_postos_deu_erro:
+    precos_gasstation, precos_distrito, precos_itirapua, precos_ppp, precos_pitstop = {}, {}, {}, {}, {}
+    for combustivel in combustiveis:
+        precos_gasstation[combustivel] = None
+        precos_distrito[combustivel] = None
+        precos_itirapua[combustivel] = None
+        precos_ppp[combustivel] = None
+        precos_pitstop[combustivel] = None
+    tempo_ex_ipr_postos = None
+
+if vibra_janjao_deu_erro:
+    precos_vbr_janjao = {}
+    for combustivel in combustiveis:
+        precos_vbr_janjao[combustivel] = None
+    tempo_ex_vbr_janjao = None
+
+
 imprime_precos(
     precos_ipr1, precos_ipr2, tempo_ex_ipr,
     precos_vbr, tempo_ex_vbr,
@@ -77,24 +146,22 @@ dados = cria_dicionario_para_df(
 
 df = pd.DataFrame(dados)
 
-# Criando um novo arquivo Excel com o nome da aba sendo a data atual
-data = datetime.now().strftime('%d-%m')
-nome_arquivo = f"C:/Users/titrr/OneDrive - MARIO ROBERTO TRANSP REVENDEDORA D OLEO DIESEL/Leva Diesel/Logística/resultados_precos.xlsx"
+arquivo = f"C:/Users/titrr/OneDrive - MARIO ROBERTO TRANSP REVENDEDORA D OLEO DIESEL/Leva Diesel/Logística/resultados_precos.xlsx"
 
 # Carregando o arquivo Excel existente (ou criando um novo se não existir)
 try:
-    writer = pd.ExcelWriter(nome_arquivo, engine='openpyxl', datetime_format='dd/mm/yyyy HH:MM:SS', date_format='dd/mm/yyyy', mode='a', if_sheet_exists='replace')
+    writer = pd.ExcelWriter(arquivo, engine='openpyxl', datetime_format='dd/mm/yyyy HH:MM:SS', date_format='dd/mm/yyyy', mode='a', if_sheet_exists='replace')
 except FileNotFoundError:
-    writer = pd.ExcelWriter(nome_arquivo, engine='openpyxl', datetime_format='dd/mm/yyyy HH:MM:SS', date_format='dd/mm/yyyy', mode='w')
+    writer = pd.ExcelWriter(arquivo, engine='openpyxl', datetime_format='dd/mm/yyyy HH:MM:SS', date_format='dd/mm/yyyy', mode='w')
 
-df.to_excel(writer, sheet_name=data, index=False)
+df.to_excel(writer, index=False)
 writer.close()
 
-print(f"Resultados salvos em: {nome_arquivo}")
+print(f"Resultados salvos em: {arquivo}")
 
 # Abrindo o arquivo Excel com openpyxl
-workbook = load_workbook(nome_arquivo)
-sheet = workbook[data]
+workbook = load_workbook(arquivo)
+sheet = workbook['Sheet1']
 
 # Inserindo uma linha acima de tudo
 sheet.insert_rows(1)
@@ -184,5 +251,5 @@ sheet['T1'].border = border
 sheet['U1'].border = border
 
 # Salvando as alterações
-workbook.save(nome_arquivo)
+workbook.save(arquivo)
 print("Excel tratado com sucesso!")
