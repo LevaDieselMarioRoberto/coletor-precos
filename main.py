0e91ac1dd4de7a99658aad706d8017dec0f04b18
jpfalcuci <jpfalcuci@gmail.com>
2024-01-17 08:42:18 -0300

    feat: Executa coleta apenas até as 20:00

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from posto import Posto
 from coletores_precos.coletor_ipiranga_trr import ColetorIpirangaTRR
 from coletores_precos.coletor_vibra_trr import ColetorVibraTRR
@@ -10,45 +11,49 @@ from telegram import Telegram
 from concurrent.futures import ThreadPoolExecutor
 from time import time
 
-ipr_trr1 = Posto("Ipiranga TRR 1")
-ipr_trr2 = Posto("Ipiranga TRR 2")
-vbr_trr = Posto("Vibra TRR")
-rzn_trr = Posto("Raízen TRR")
-ipr_gasstation = Posto("Ipiranga Gas Station")
-ipr_distrito = Posto("Ipiranga Distrito")
-ipr_itirapua = Posto("Ipiranga Itirapuã")
-ipr_ppp = Posto("Ipiranga PPP")
-ipr_pitstop = Posto("Ipiranga Pit Stop")
-vbr_jj = Posto("Vibra Janjão")
+if datetime.now().hour < 20:
 
-coletor_ipr_trr = ColetorIpirangaTRR()
-coletor_vbr_trr = ColetorVibraTRR()
-coletor_rzn_trr = ColetorRaizenTRR()
-coletor_ipr_postos = ColetorIpirangaPostos()
-coletor_vbr_jj = ColetorVibraJanjao()
+    ipr_trr1 = Posto("Ipiranga TRR 1")
+    ipr_trr2 = Posto("Ipiranga TRR 2")
+    vbr_trr = Posto("Vibra TRR")
+    rzn_trr = Posto("Raízen TRR")
+    ipr_gasstation = Posto("Ipiranga Gas Station")
+    ipr_distrito = Posto("Ipiranga Distrito")
+    ipr_itirapua = Posto("Ipiranga Itirapuã")
+    ipr_ppp = Posto("Ipiranga PPP")
+    ipr_pitstop = Posto("Ipiranga Pit Stop")
+    vbr_jj = Posto("Vibra Janjão")
 
-logger = Logger()
-telegram = Telegram()
+    coletor_ipr_trr = ColetorIpirangaTRR()
+    coletor_vbr_trr = ColetorVibraTRR()
+    coletor_rzn_trr = ColetorRaizenTRR()
+    coletor_ipr_postos = ColetorIpirangaPostos()
+    coletor_vbr_jj = ColetorVibraJanjao()
 
-funcoes_e_argumentos = [    # Lista de tuplas, sendo a função e seus respectivos argumentos
-    (coletor_ipr_trr.coleta_precos, [ipr_trr1, ipr_trr2]),
-    (coletor_vbr_trr.coleta_precos, [vbr_trr]),
-    (coletor_rzn_trr.coleta_precos, [rzn_trr]),
-    (coletor_vbr_jj.coleta_precos, [vbr_jj]),
-    (coletor_ipr_postos.coleta_precos, [ipr_gasstation, ipr_distrito, ipr_itirapua, ipr_ppp, ipr_pitstop]),
-]
+    logger = Logger()
+    telegram = Telegram()
 
-inicio = time()
-logger.log(f"   --- Iniciando coletas... ---")
-with ThreadPoolExecutor() as executor:  # Executa as funções simultaneamente
-    executor.map(lambda x: x[0](*x[1]), funcoes_e_argumentos)
-tempo_total = round(time() - inicio, 2)
-logger.log(f"Tempo de execução: {tempo_total} segundos")
+    funcoes_e_argumentos = [    # Lista de tuplas, sendo a função e seus respectivos argumentos
+        (coletor_ipr_trr.coleta_precos, [ipr_trr1, ipr_trr2]),
+        (coletor_vbr_trr.coleta_precos, [vbr_trr]),
+        (coletor_rzn_trr.coleta_precos, [rzn_trr]),
+        (coletor_vbr_jj.coleta_precos, [vbr_jj]),
+        (coletor_ipr_postos.coleta_precos, [ipr_gasstation, ipr_distrito, ipr_itirapua, ipr_ppp, ipr_pitstop]),
+    ]
 
-postos = [ipr_pitstop, ipr_distrito, ipr_gasstation, ipr_itirapua, ipr_ppp, vbr_jj, ipr_trr1, ipr_trr2, vbr_trr, rzn_trr,]
-# for posto in postos: posto.imprime_precos()
+    inicio = time()
+    logger.log(f"   --- Iniciando coletas... ---")
 
-planilha = Planilha()
-planilha.salva_planilha(postos)
+    with ThreadPoolExecutor() as executor:  # Executa as funções simultaneamente
+        executor.map(lambda x: x[0](*x[1]), funcoes_e_argumentos)
 
-for posto in postos: posto.compara_precos()
+    tempo_total = round(time() - inicio, 2)
+    logger.log(f"Tempo de execução: {tempo_total} segundos")
+
+    postos = [ipr_pitstop, ipr_distrito, ipr_gasstation, ipr_itirapua, ipr_ppp, vbr_jj, ipr_trr1, ipr_trr2, vbr_trr, rzn_trr,]
+    # for posto in postos: posto.imprime_precos()
+
+    planilha = Planilha()
+    planilha.salva_planilha(postos)
+
+    for posto in postos: posto.compara_precos()
