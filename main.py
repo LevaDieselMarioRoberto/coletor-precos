318bc9ef000a720114cde04cb95bd2838d4b5b76
jpfalcuci <jpfalcuci@gmail.com>
2024-06-03 13:18:15 -0300

    feat: control variables

--- main.py
+++ main.py
@@ -7,40 +7,61 @@ from coletores_precos.coletor_vibra_janjao import ColetorVibraJanjao
 from coletores_precos.coletor_ipiranga_postos import ColetorIpirangaPostos
 from planilha import Planilha
 from logger import Logger
-from telegram import Telegram
 from concurrent.futures import ThreadPoolExecutor
 from time import time
+from config import CONTROLS
 
 
-if datetime.now().hour < 20:
+logger = Logger()
 
-    ipr_trr1 = Posto("Ipiranga TRR 1")
-    ipr_trr2 = Posto("Ipiranga TRR 2")
-    vbr_trr = Posto("Vibra TRR")
-    rzn_trr = Posto("Raízen TRR")
-    ipr_gasstation = Posto("Ipiranga Gas Station")
-    ipr_distrito = Posto("Ipiranga Distrito")
-    ipr_itirapua = Posto("Ipiranga Itirapuã")
-    ipr_ppp = Posto("Ipiranga PPP")
-    ipr_pitstop = Posto("Ipiranga Pit Stop")
-    vbr_jj = Posto("Vibra Janjão")
+if CONTROLS['COLETA_HABILITADA'] and datetime.now().hour < 20:
 
-    coletor_ipr_trr = ColetorIpirangaTRR()
-    coletor_vbr_trr = ColetorVibraTRR()
-    coletor_rzn_trr = ColetorRaizenTRR()
-    coletor_ipr_postos = ColetorIpirangaPostos()
-    coletor_vbr_jj = ColetorVibraJanjao()
+    funcoes_e_argumentos = [] # Lista de tuplas, sendo a função e seus respectivos argumentos
 
-    logger = Logger()
-    telegram = Telegram()
+    if CONTROLS['COLETAR_IPR_TRR']:
+        ipr_trr1 = Posto("Ipiranga TRR 1")
+        ipr_trr2 = Posto("Ipiranga TRR 2")
+        coletor_ipr_trr = ColetorIpirangaTRR()
+        funcoes_e_argumentos.append((
+            coletor_ipr_trr.coleta_precos,
+            [ipr_trr1, ipr_trr2, CONTROLS['MAXIMIZADO_IPR_TRR']]
+        ))
+    
+    if CONTROLS['COLETAR_VBR_TRR']:
+        vbr_trr = Posto("Vibra TRR")
+        coletor_vbr_trr = ColetorVibraTRR()
+        funcoes_e_argumentos.append((
+            coletor_vbr_trr.coleta_precos,
+            [vbr_trr, CONTROLS['MAXIMIZADO_VBR_TRR']]
+        ))
 
-    funcoes_e_argumentos = [    # Lista de tuplas, sendo a função e seus respectivos argumentos
-        (coletor_ipr_trr.coleta_precos, [ipr_trr1, ipr_trr2]),
-        (coletor_vbr_trr.coleta_precos, [vbr_trr]),
-        (coletor_rzn_trr.coleta_precos, [rzn_trr]),
-        (coletor_vbr_jj.coleta_precos, [vbr_jj]),
-        (coletor_ipr_postos.coleta_precos, [ipr_gasstation, ipr_distrito, ipr_itirapua, ipr_ppp, ipr_pitstop]),
-    ]
+    if CONTROLS['COLETAR_RZN_TRR']:
+        rzn_trr = Posto("Raízen TRR")
+        coletor_rzn_trr = ColetorRaizenTRR()
+        funcoes_e_argumentos.append((
+            coletor_rzn_trr.coleta_precos,
+            [rzn_trr, CONTROLS['MAXIMIZADO_RZN_TRR']]
+        ))
+
+    if CONTROLS['COLETAR_IPR_POSTOS']:
+        ipr_gasstation = Posto("Ipiranga Gas Station")
+        ipr_distrito = Posto("Ipiranga Distrito")
+        ipr_itirapua = Posto("Ipiranga Itirapuã")
+        ipr_ppp = Posto("Ipiranga PPP")
+        ipr_pitstop = Posto("Ipiranga Pit Stop")
+        coletor_ipr_postos = ColetorIpirangaPostos()
+        funcoes_e_argumentos.append((
+            coletor_ipr_postos.coleta_precos,
+            [ipr_gasstation, ipr_distrito, ipr_itirapua, ipr_ppp, ipr_pitstop, CONTROLS['MAXIMIZADO_IPR_POSTOS']]
+        ))
+    
+    if CONTROLS['COLETAR_VBR_JJ']:
+        vbr_jj = Posto("Vibra Janjão")
+        coletor_vbr_jj = ColetorVibraJanjao()
+        funcoes_e_argumentos.append((
+            coletor_vbr_jj.coleta_precos,
+            [vbr_jj, CONTROLS['MAXIMIZADO_VBR_JJ']]
+        ))
 
     inicio = time()
     logger.log(f"   --- Iniciando coletas... ---")
@@ -52,9 +73,16 @@ if datetime.now().hour < 20:
     logger.log(f"Tempo de execução: {tempo_total} segundos")
 
     postos = [ipr_pitstop, ipr_distrito, ipr_gasstation, ipr_itirapua, ipr_ppp, vbr_jj, ipr_trr1, ipr_trr2, vbr_trr, rzn_trr,]
-    # for posto in postos: posto.imprime_precos()
 
-    planilha = Planilha()
-    planilha.salva_planilha(postos)
+    if CONTROLS['IMPRIME_PRECOS_TERMINAL']:
+        for posto in postos: posto.imprime_precos()
 
-    for posto in postos: posto.compara_precos()
+    if CONTROLS['SALVA_PRECOS_PLANILHA']:
+        planilha = Planilha()
+        planilha.salva_planilha(postos)
+
+    if CONTROLS['ENVIA_PRECOS_TELEGRAM']:
+        for posto in postos: posto.compara_precos()
+
+else:
+    logger.log("Coleta desabilitada ou fora do horário permitido.")
